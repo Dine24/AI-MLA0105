b91377aa3faeb1ddf7b84bac37fbc2ac83c11d04
Dinesha G <120408570+Dine24@users.noreply.github.com>
2023-11-23 10:13:10 +0530

    Add files via upload

--- Create a Web Blog using Word press to demonstrate Anchor Tag, Title Tag, etc.pl
+++ Create a Web Blog using Word press to demonstrate Anchor Tag, Title Tag, etc.pl
@@ -1,2 +1,21 @@
-<a href="https://www.example.com">Example Website</a>
-    <a href="https://www.example.com" title="Visit Example Website">Example Website</a>
+% Facts representing blog posts
+post(1, 'My First Blog Post', 'This is my first blog post. Excited to share thoughts!').
+post(2, 'Exploring Prolog', 'Learning Prolog is an interesting journey.').
+
+% Facts representing comments on blog posts
+comment(1, 'Great post!').
+comment(1, 'Looking forward to more content.').
+comment(2, 'Prolog is fascinating!').
+
+% Rules to display posts and comments
+display_post(PostID) :-
+    post(PostID, Title, Content),
+    format('Title: ~w~n', [Title]),
+    format('Content: ~w~n', [Content]),
+    display_comments(PostID),
+    nl.
+
+display_comments(PostID) :-
+    comment(PostID, Comment),
+    format('  Comment: ~w~n', [Comment]),
+    fail. % Force backtracking to find more comments
